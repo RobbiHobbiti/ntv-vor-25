6d6f2f00d3dee7f47cbe9bbd5e30865c42fc7beb
RobbiHobbiti <Robbihobbiti@gmail.com>
2025-02-19 20:47:49 +0000

    BUIÐ

--- cheatSheet/class01.dart
+++ cheatSheet/class01.dart
@@ -7,30 +7,31 @@ void main(List<String> arguments) {
     message to that person. Your message should be simple, such as 'Good morning
     Hjörtur, would you like to learn Dart strings today?'
     Store Message and Name in seperate variable.
-
+*/
   print("Hvað heitir þú?");
   String firstName = stdin.readLineSync().toString();
   String message = "Ertu Glaður?";
   print("Góðan dægin $firstName $message");
-  */
+
   /* 1. Find a quote from a famous person you admire. Print the quote and the name of its
   author. Your output should look something like the following, including the
   quotation marks:
   1. Albert Einstein once said, 'A person who never made a mistake never tried
   anything new.'
+*/
 
-
-  String name = "dr seuss";
+  String name = "Dr. Seuss";
   String msg = "You know you're in love when you can't fall asleep because reality is finally better than your dreams.";
   print("$name once said:$msg");
-  */
+
   /*
     2. Assign a message to a variable, and print that message. Then change the value of
   the variable to a new message, and print the new message.
    */
-  String message = "Hello";
-  message = "bless";
-  print("$message");
+  String Msg = "Hello";
+  print("$Msg");
+  Msg = "bless";
+  print("$Msg");
   /*
     3.  Remove all the spaces from the rhyme. And print the modified rhyme.
    */
@@ -46,14 +47,14 @@ void main(List<String> arguments) {
     first letter, and the other letters being lowercase.
    */
   print("whats your first name?");
-  String firstName = stdin.readLineSync().toString();
+  String FirstName = stdin.readLineSync().toString();
   print("whats your last name?");
   String lastName = stdin.readLineSync().toString();
-  String fullName = firstName + " " + lastName;
+  String fullName = FirstName + " " + lastName;
   print("Hello $fullName".toUpperCase());
   String fixName = "Róbert";
   print("hallo $fixName $lastName");
-  print("${firstName[0].toUpperCase()}${firstName.substring(1)} ${lastName[0].toUpperCase()}${lastName.substring(1)}");
+  print("${FirstName[0].toUpperCase()}${FirstName.substring(1).toLowerCase()} ${lastName[0].toUpperCase()}${lastName.substring(1).toLowerCase()}");
 
   /*
     5. Display the following SSN on the correct Format:
@@ -74,5 +75,6 @@ void main(List<String> arguments) {
   /*
   Bónus task calculate the age from the SSN.
    */
+
   
 }
